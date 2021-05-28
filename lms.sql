d6a2a6dcb08fdbfb0c93432bc1f29b2cc027ee37
FXLJA <ja669543@gmail.com>
2021-05-29 01:56:13 +0700

    export new database file

--- lms.sql
+++ lms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2021 at 10:04 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Generation Time: May 28, 2021 at 08:55 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,9 +38,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_contact`) VALUES
-('001', 'Jaya', 'Jayaku.com'),
-('542534', '65', '456456'),
-('ferdy2', 'ferdy', 'over there');
+('32180018', 'Ferdy Nicolas', 's32180018@ubm.com');
 
 -- --------------------------------------------------------
 
@@ -54,6 +52,17 @@ CREATE TABLE `student` (
   `student_class` varchar(10) DEFAULT NULL,
   `student_major` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `student_name`, `student_class`, `student_major`) VALUES
+('S-0001', 'Hendrik', '6PTI', 'IPA'),
+('S-0015', 'Garvin Adrian', '6PTI', 'IPS'),
+('S-0036', 'Muhammad Nur Salim', '6PTI', 'IPS'),
+('S-0082', 'Jason Alexander', '6PTI', 'IPA'),
+('S-0118', 'Ryan Nugraha', '6PTI', 'IPA');
 
 -- --------------------------------------------------------
 
@@ -77,6 +86,7 @@ INSERT INTO `subject` (`subject_id`, `subject_name`, `subject_major`, `subject_m
 ('BIND', 'Bahasa Indonesia', 'Umum', 80),
 ('BING', 'Bahasa Inggiris', 'Umum', 40),
 ('BIPA', 'Biologi', 'IPA', 80),
+('BKSL', 'Bimbingan Konseling', 'Umum', 40),
 ('EIPS', 'Ekonomi', 'IPS', 80),
 ('FIPA', 'Fisika', 'IPA', 80),
 ('GIPS', 'Geografi', 'IPS', 80),
@@ -85,9 +95,11 @@ INSERT INTO `subject` (`subject_id`, `subject_name`, `subject_major`, `subject_m
 ('MTKP', 'Matematika Pilihan', 'Umum', 40),
 ('MTKU', 'Matematika Umum', 'Umum', 80),
 ('ORKJ', 'Olahraga dan Kesehatan Jasmani', 'Umum', 80),
+('PKWN', 'Pancasila & Kewarganegaraan', 'Umum', 80),
 ('SBDK', 'Seni Budaya dan Keterampilan', 'Umum', 40),
 ('SIPS', 'Sosiologi', 'IPS', 80),
-('SJRH', 'Sejarah', 'Umum', 40);
+('SJRH', 'Sejarah', 'Umum', 40),
+('SNMK', 'Seni Musik', 'Umum', 80);
 
 -- --------------------------------------------------------
 
@@ -106,10 +118,23 @@ CREATE TABLE `teacher` (
 --
 
 INSERT INTO `teacher` (`teacher_id`, `teacher_name`, `teacher_subject`) VALUES
-('765765', 'Derry Leo Nardi', 'SIPS'),
-('Daniel', 'Daniel Ronaldo', 'MTKU'),
-('felir', 'Felix Dermawan', 'AGET'),
-('jinto', 'Jayaku Brilliantio', 'EIPS');
+('T-0014', 'Jet J Krisnadi', 'EIPS'),
+('T-0028', 'Byron Harnope', 'BKSL'),
+('T-0033', 'Michael Freddy', 'KTIK'),
+('T-0066', 'Felix Dermawan', 'AGET'),
+('T-0074', 'Maikel', 'SBDK'),
+('T-0075', 'Willy Stevanus Susilo', 'GIPS'),
+('T-0076', 'Kalingga Karamitta Gunawan', 'SJRH'),
+('T-0082', 'Jason Alexander', 'BIPA'),
+('T-0084', 'Vincent', 'PKWN'),
+('T-0086', 'Derry Leo Nardi', 'BIND'),
+('T-0088', 'Jayaku Brilliantio', 'FIPA'),
+('T-0091', 'Daniel Ronaldo Pangestu', 'BING'),
+('T-0105', 'Fernando', 'MTKU'),
+('T-0112', 'Arvin Christopher', 'SNMK'),
+('T-0114', 'Kevin Hosea', 'MTKP'),
+('T-0115', 'Daniel', 'KIPA'),
+('T-0134', 'Martien Junaedi', 'ORKJ');
 
 -- --------------------------------------------------------
 
@@ -128,16 +153,29 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `password`, `role`) VALUES
-('001', 'jayaku', 'Admin'),
-('542534', '546456', 'Admin'),
-('765765', '123', 'Teacher'),
-('Daniel', '123', 'Teacher'),
-('felir', 'dewa', 'Teacher'),
-('Ferdy', '123', 'Student'),
-('ferdy2', '123', 'Admin'),
-('ferdy3', '123', 'Teacher'),
-('jayaku', '123', 'Teacher'),
-('jinto', 'dogecoin', 'Teacher');
+('32180018', 'ferdy', 'Admin'),
+('S-0001', 'napadiam', 'Student'),
+('S-0015', 'garvin', 'Student'),
+('S-0036', 'salim', 'Student'),
+('S-0082', 'jason', 'Student'),
+('S-0118', 'yanu', 'Student'),
+('T-0014', 'jet', 'Teacher'),
+('T-0028', 'byron', 'Teacher'),
+('T-0033', 'michael', 'Teacher'),
+('T-0066', 'felix', 'Teacher'),
+('T-0074', 'maikel', 'Teacher'),
+('T-0075', 'willy', 'Teacher'),
+('T-0076', 'kalingga', 'Teacher'),
+('T-0082', 'jason', 'Teacher'),
+('T-0084', 'vincent', 'Teacher'),
+('T-0086', 'derry', 'Teacher'),
+('T-0088', 'jayaku', 'Teacher'),
+('T-0091', 'daniel', 'Teacher'),
+('T-0105', 'genshin', 'Teacher'),
+('T-0112', 'guppy', 'Teacher'),
+('T-0114', 'cibai', 'Teacher'),
+('T-0115', 'rajin', 'Teacher'),
+('T-0134', 'martien', 'Teacher');
 
 --
 -- Indexes for dumped tables
